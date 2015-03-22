128ce86cec69f675ff3247df58abb51e4ca9c5fb
Yuri Camara <yuricamara@gmail.com>
2015-03-22 13:57:59 -0300

    Add build task to concat and minify css files

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,6 +56,28 @@ module.exports = function(grunt){
           mainConfigFile:'src/js/main.js'
         }
       }
+    },
+    concat:{
+      build:{
+        files:{
+          'public/styles.css':[
+            'src/vendors/bower_components/normalize.css/normalize.css',
+            'src/css/stylesheet.css'
+          ]
+        }
+      }
+    },
+    cssmin:{
+      build:{
+        files:{
+          'public/styles.min.css': 'public/styles.css'
+        }
+      }
+    },
+    clean:{
+      build:{
+        src: 'public/styles.css'
+      }
     }
   });
 
@@ -73,6 +95,7 @@ module.exports = function(grunt){
   grunt.registerTask('js-tests-report', function(){
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-connect');
+
     grunt.task.run('copy:tests');
     grunt.task.run('connect:tests');
   });
@@ -81,6 +104,9 @@ module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-processhtml');
     grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
     //images
     grunt.task.run('copy:build');
@@ -88,6 +114,10 @@ module.exports = function(grunt){
     //scripts.min.js
     grunt.task.run('requirejs:build');
 
+    //styles.min.css
+    grunt.task.run('concat:build');
+    grunt.task.run('cssmin:build');
+    grunt.task.run('clean:build');
 
     //index.html
     grunt.task.run('processhtml:build');
